7c47d1f5bdf5bb08df169e339c8e59746b0ebcd6
Philippe Van de Vyver <philippe.vandevyver@tillit.be>
2025-10-05 18:53:50 +0200

    add dotenv again

--- _webapp/app/__init__.py
+++ _webapp/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
 import urllib.parse
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 login_manager = LoginManager()
