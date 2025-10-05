9a77261d6a9cf89e6671bbbf553ca64e64501415
Barre D'Haeseleer <barre.dhaeseleer@tillit.be>
2025-10-05 19:03:22 +0200

    init.py fix

--- _webapp/app/__init__.py
+++ _webapp/app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 import os
 import urllib.parse
 from dotenv import load_dotenv
 load_dotenv()
 
 db = SQLAlchemy()
-login_manager = LoginManager()
 
 
 def _odbc_brace(v: str) -> str:
@@ -40,25 +38,12 @@ def create_app():
         "pool_pre_ping": True, "pool_recycle": 300}
 
     db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = "auth.login"
+
 
     from .auth import auth_bp
-    from .results import results_bp
-    from .admin import admin_bp
     from .routes import main_bp
-    from .video import video_bp
-    app.register_blueprint(main_bp)
-    app.register_blueprint(video_bp)
-    app.register_blueprint(admin_bp)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(results_bp)
 
-    from .stream_worker import StreamWorker
-    # note: 192.168.x.x won't be reachable from Azure; consider a public or VNet-reachable endpoint
-    stream_worker = StreamWorker(
-        app, rtmp_url="rtmp://192.168.151.16:1935/live/stream")
-    stream_worker.start()
-    app.stream_worker = stream_worker
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
 
     return app
